d45fa9e20daacae3c76dab9086675aba9bbd7ffe
Oscar19260 <oscar19260@gmail.com>
2023-02-09 14:16:40 -0600

    feat: update

--- pages/hireme.js
+++ pages/hireme.js
@@ -2,7 +2,8 @@ import Layout from "../components/Layout";
 
 const Hireme = () => (
   <Layout footer={false}>
-    <h1 className="text-center"> Please. Email me.</h1>
+    <h1 className="text-center"> Please email me</h1>
+    <br></br>
     <h1 className="text-center"> oscar19260@gmail.com</h1>
   </Layout>
 );
